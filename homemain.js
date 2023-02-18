19c66ebbbb752ce710c3650984a07ea0eed3779e
Saint-Dave <babaloladavid7@gmail.com>
2023-02-18 13:18:20 +0100

    removed some codes from js

--- homemain.js
+++ homemain.js
@@ -1,13 +1,3 @@
-// Top Menu Navigation
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
 //   Quotes js
   const quotes = [{
     quote: `"You only live once, but if you do it right, once is enough."`,
